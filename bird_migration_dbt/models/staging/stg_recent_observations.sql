17049debcb8828f74deaf3dd0ac6b868b58ea74f
charlesrobison <crobison@uw.edu>
2025-08-08 10:48:54 -0400

    Update aliases and descriptions

--- bird_migration_dbt/models/staging/stg_recent_observations.sql
+++ bird_migration_dbt/models/staging/stg_recent_observations.sql
@@ -1,7 +1,31 @@
 {{ config(materialized='table') }}
 
 with final as (
-    select *
+    select 
+        speciesCode as species_code,
+        comName as common_name,
+        sciName as scientific_name,
+        locId as location_id,
+        locName as location_name,
+        obsDt as observation_datetime,
+        howMany as observation_count,
+        lat as latitude,
+        lng as longitude,
+        obsValid as observation__is_valid,
+        obsReviewed as observation__is_reviewed,
+        locationPrivate as location__is_private,
+        subId as submitted_by_id,
+        exoticCategory as exotic_category,
+        fetch_timestamp as fetch_datetime,
+        md5(
+            concat_ws('||',
+                submitted_by_id,
+                observation_datetime,
+                species_code,
+                location_id
+            )
+        ) as observation_id
+
     from read_csv_auto('data/recent_bird_obs.csv')
 )
 
